99024e8b3992fe9a8e515fdcaa23ed0ba3ee7dc8
geeky-arn4v <115574150+geeky-arn4v@users.noreply.github.com>
2026-02-08 22:13:09 -0600

    add

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,8 +1,30 @@
 #include "three_sum_sorted.h"
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
+if(!nums || !out_i || !out_j||!out_k|| n<3) return 0;
+for (int i = 0; i < n-2; i++){
+    int j=i+1;
+    int k =n-1;
+    while (j<k)
+    {
+      int sum= nums[i]+nums[j]+nums[j];
+      
+      if(sum==0){
+        *out_i=i;
+        *out_j=j;
+        *out_k=k;
+        return 1;
+      }
+      else if (sum<0){
+        j++;
+      } 
+      else {
+        k--;
+      }
+    }
     
-    // implement your code here
+}
+
 
     return 0;
 }
--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -1,14 +1,17 @@
 #include "two_sum.h"
-
+//[2,7,11,15]
 int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
+    for(int i=0; i<n;i++){
+        for(int j=i+1; j<n; j++){
+            if(nums[i]+nums[j]==target){
+                *out_i=i;
+                *out_j=j;
+                return 1;
+            }
 
-    // TODO: implement Two Sum.
-    // Requirements:
-    //  - find i < j such that nums[i] + nums[j] == target
-    //  - store indices in *out_i and *out_j
-    //  - return 1 if found; otherwise return 0
+        }
+
+    }
 
     return 0;
 }
-
-//think about the time complexity and space complexity of your solution
